0cdad4d1c51ffeb940652ccbb174a36de9763dc5
yashkarpe777 <yashkarpebsc@gmail.com>
2025-10-25 13:29:55 +0530

    Apply local changes to StitchCar backend (apps.py updates and new StitchCar files)

--- StitchCarbackend/StitchCar/settings.py
+++ StitchCarbackend/StitchCar/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'StitchCarbackendapp',
+    'StitchCarbackend.StitchCarbackendapp',
     'rest_framework',
     'graphene_django',
     'corsheaders'
