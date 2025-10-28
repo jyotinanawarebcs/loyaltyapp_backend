5268df3d0bb196b5bc3a01527ec11eb557d7041c
yashkarpe777 <yashkarpebsc@gmail.com>
2025-10-28 14:06:18 +0530

    latest changes

--- StitchCarbackend/StitchCar/settings.py
+++ StitchCarbackend/StitchCar/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'StitchCarbackend.StitchCarbackendapp',
+    'StitchCarbackendapp',
     'rest_framework',
     'graphene_django',
     'corsheaders'
